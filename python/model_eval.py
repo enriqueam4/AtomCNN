6d98d0d0586dcd6468fb87f64baf52aadab2ace4
enriqueam4 <72879771+enriqueam4@users.noreply.github.com>
2021-07-13 18:03:11 -0400

    Incomplete: Dependent on changes in other files

--- python/model_eval.py
+++ python/model_eval.py
@@ -5,10 +5,20 @@ import h5py
 import netclasses
 import torch.nn as nn
 
+
 def main():
+    try:
+        torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
+        device = torch.cuda.get_device_name(0)
+        print('Available GPU:', str(device))
+    except RuntimeError:
+        print('Please make sure you selected GPU as a hardware accelerator in Runtime --> Change runtime type')
+        return
+
     criterion = nn.BCELoss()
-    h5_filename = r""
-    weights_filename = r""
+    h5_filename = r"C:\Users\emejia\Desktop\AtomCNN\h5_files\Zdump.h5"
+    weights_filename = r"C:\Users\emejia\Desktop\AtomCNN\models\test1-1-best_weights.pt"
     h5 = h5py.File(h5_filename)
     print(list(h5.keys()))
     checkpoint = torch.load(weights_filename, map_location='cpu')
@@ -16,18 +26,33 @@ def main():
     model.load_state_dict(checkpoint)
     print("model initiated")
     score = []
-    defocus = []
-    for i in range(len(h5["X_train"])):
-        image = h5["X_Train"][i]
-        image_tensor = torch.empty((1, 1, image.shape[0], image.shape[1]))
-        image_tensor[0, 0, :, :] = image
-        prediction = np.asarray(model.forward(image_tensor))
-        ground_truth = np.asarray(h5["y_train"][i])
-        loss = criterion(prediction, ground_truth)
-        score.append(correctness)
-        metadata = jsonpickle.decode(h5["Z_train"][i])
-        defocus.append(metadata[])
+    metarray = []
+    print(list(h5.keys()))
+    n_train = len(h5["X_train"])
+    n_train = 100
+
+    for i in range(n_train):
+        image = np.expand_dims(h5["X_train"][i], axis=0)
+        image_tensor = torch.from_numpy(image).float()
+        forward = model.forward(image_tensor)
+        prediction = forward.cpu().detach().numpy()[0, 0, :, :]
+        ground_truth = np.asarray(h5["y_train"][i])[0, :, :]
+        gt_tensor = torch.from_numpy(image).float()
+        loss = criterion(forward, gt_tensor)
+        loss = loss.item()
+        print(loss)
+        score.append(loss)
+        meta = h5["Z_test"]
+        metadata = jsonpickle.decode(h5["Z_test"][i])
+        metarray.append(metadata)
+
+    for i in range(len(metarray)):
+        defocus = metarray[i]
+
+    plt.plot(np.asarray(score), np.asarray)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
+
+# Enrique Mejia
