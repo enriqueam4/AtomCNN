cb3c2074478495e8e13ec290c0cba17dd8c481b1
enriqueam4 <72879771+enriqueam4@users.noreply.github.com>
2021-07-12 20:57:58 -0400

    Trained model evaluation program - incomplete

---
+++ python/model_eval.py
@@ -0,0 +1,32 @@
+import numpy as np
+import jsonpickle
+import torch
+import h5py
+import netclasses
+
+
+def main():
+    h5_filename = r""
+    weights_filename = r""
+    h5 = h5py.File(h5_filename)
+    print(list(h5.keys()))
+    checkpoint = torch.load(weights_filename, map_location='cpu')
+    model = netclasses.atomsegnet()
+    model.load_state_dict(checkpoint)
+    print("model initiated")
+    score = []
+    defocus = []
+    for i in range(len(h5["X_train"])):
+        image = h5["X_Train"][i]
+        image_tensor = torch.empty((1, 1, image.shape[0], image.shape[1]))
+        image_tensor[0, 0, :, :] = image
+        prediction = np.asarray(model.forward(image_tensor))
+        ground_truth = np.asarray(h5["y_train"][i])
+        correctness = np.sum(abs(prediction - ground_truth))
+        score.append(correctness)
+        metadata = jsonpickle.decode(h5["Z_train"][i])
+        defocus.append(metadata[])
+
+
+if __name__=="__main__":
+    main()
