1c163875fba9331fdbfe6edeca60889ae67935cf
enriqueam4 <72879771+enriqueam4@users.noreply.github.com>
2021-07-21 15:36:20 -0400

    Added command line updates, plots, loss

--- python/Local_Train.py
+++ python/Local_Train.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 import torch
 import netclasses
 import torch.optim as optim
 import os
 import sys
+import numpy as np
 
 
 def main():
@@ -18,6 +20,7 @@ def main():
     hf_file = sys.argv[1]
     model_directory = sys.argv[2]
     epochs = int(sys.argv[3])
+    epoch_array = range(epochs)
 
     batch_size = 30
     gen = netclasses.generate_batches(hf_file, batch_size)
@@ -27,7 +30,17 @@ def main():
     model = netclasses.atomsegnet()
     model.cuda()
     optimizer = optim.Adam(model.parameters(), lr=5e-4)
-    netclasses.train(epochs=epochs, hf_file=hf_file, model=model, optimizer=optimizer, batch_size=batch_size, model_directory=model_directory)
+    train_losses, test_losses = netclasses.train(epochs=epochs, hf_file=hf_file, model=model, optimizer=optimizer,
+                                                 batch_size=batch_size, model_directory=model_directory)
+
+    np.save(model_directory+"train_losses.npy", train_losses)
+    np.save(model_directory+"train_losses.npy", train_losses)
+    np.save(model_directory+"train_losses.npy", epoch_array)
+    plt.scatter(epoch_array, train_losses)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Loss")
+    plt.savefig(model_directory+"loss.png")
 
 
 if __name__ == '__main__':
